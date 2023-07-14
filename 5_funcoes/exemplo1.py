70d68ec19ab142c86f6cd674f6bee48fd17f7c9f
Enzo Alves <enzoviniciusalves12@gmail.com>
2023-07-14 14:58:08 -0300

    adicao de exemplos de aula de funcoes

---
+++ 5_funcoes/exemplo1.py
@@ -0,0 +1,6 @@
+def somar_numeros(num1, num2):
+    soma = num1 + num2
+    return soma
+resultado = somar_numeros(5, 3)
+print(resultado)  # O resultado será 8
+
---
+++ 5_funcoes/exemplo2.py
@@ -0,0 +1,9 @@
+def questao1(numero):
+    resultado = ""
+    for n in range(1, numero + 1):
+        for i in range(1, n + 1):
+            resultado += f"{str(n)}"
+        resultado += "\n"
+    return resultado
+teste = int(input("Digite seu numero: "))
+print(questao1(teste))
